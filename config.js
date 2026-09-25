fcf122f22e6d6b04813797799650ace25f5e7c22
TemiOyeyipo <onelchaim@gmail.com>
2026-09-25 14:46:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,10 @@
  * Whenever you redeploy a NEW deployment (not just a new version of an
  * existing one), the URL changes and this file needs updating.
  */
-const CONFIG = 
-  API_URL: 'https://script.google.com/macros/s/AKfycbw0m9jlzvZ45-V6ET_TTJt2IUWWVhj8mt8x2Kv6NaHFuPxYzZSwFJsr6LLIxxXFTJj0/exec'
 
+const CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbw0m9jlzvZ45-V6ET_TTJt2IUWWVhj8mt8x2Kv6NaHFuPxYzZSwFJsr6LLIxxXFTJj0/exec'
+};
 
 
 
